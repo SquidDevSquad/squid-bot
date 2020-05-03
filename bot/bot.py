0c94f48a977306a84420c07109671c723471a863
Kane <mormerhav@gmail.com>
2020-05-03 17:57:35 +0300

    Improve logging format + add logs

--- bot/bot.py
+++ bot/bot.py
@@ -1,18 +1,21 @@
 from discord.ext import commands
 
 from file.FileRepository import *
+from log import LoggerFactory
+
+log = LoggerFactory.get_logger(__name__)
 
 client = commands.Bot(command_prefix=Config.COMMAND_PREFIX)
 
 for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
-        print('Load: ' + filename)
+        log.info('Load: ' + filename)
         client.load_extension(f'cogs.{filename[:-3]}')
 
 
 @client.event
 async def on_ready():
-    print("Bot started")
+    log.info("Bot started")
 
 @client.event
 async def on_command_error(ctx, error):
@@ -25,7 +28,7 @@ async def load(ctx, extension):
     if not is_admin(ctx.author.id):
         await ctx.send('You don\'t have the permissions to use this command')
         return
-    print('Load: ' + extension)
+    log.info('Load: ' + extension)
     client.load_extension(f'cogs.{extension}')
 
 @client.command(description="Unloads an extension")
@@ -33,7 +36,7 @@ async def unload(ctx, extension):
     if not is_admin(ctx.author.id):
         await ctx.send('You don\'t have the permissions to use this command')
         return
-    print('Unload: ' + extension)
+    log.info('Unload: ' + extension)
     client.unload_extension(f'cogs.{extension}')
 
 @client.command(description="Reloads an extension")
@@ -41,7 +44,7 @@ async def reload(ctx, extension):
     if not is_admin(ctx.author.id):
         await ctx.send('You don\'t have the permissions to use this command')
         return
-    print('Reload: ' + extension)
+    log.info('Reload: ' + extension)
     client.unload_extension(f'cogs.{extension}')
     client.load_extension(f'cogs.{extension}')
 
--- bot/log/LoggerFactory.py
+++ bot/log/LoggerFactory.py
@@ -1,6 +1,6 @@
 import logging
 
-formatter = logging.Formatter(fmt='%(asctime)s - %(name)s :: %(funcName)20s() - [%(levelname)s] - %(message)s')
+formatter = logging.Formatter(fmt='%(asctime)s - %(name)s :: %(funcName)10s() - [%(levelname)s] - %(message)s')
 handler = logging.StreamHandler()
 handler.setFormatter(formatter)
 
