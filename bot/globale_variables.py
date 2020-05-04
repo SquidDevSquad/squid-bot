622e1aa0e2cf802c8532ee2d61f756ad252c0fd8
Seyuna <Seyuna.Tera@gmail.com>
2020-05-04 21:16:05 +0200

    removed debugging stuff

--- bot/globale_variables.py
+++ bot/globale_variables.py
@@ -1,12 +1,11 @@
 class GlobaleVariables:
     def __init__(self):
-        # self.players_list = list()
-        self.players_list = ['Nyxii', 'Useless', 'S4lt1', 'AngelSaphire', 'Doritos', 'Blaid', 'Kane', 'djmanush', 'silverwolf', 'Ice', 'TDunker', 'Grisley', 'PingPing', 'Kitty', 'Firetruck']
+        self.players_list = list()
         self.players_allowed_to_play = list()
         self.already_used_index = list()
         self.teams = list()
         self.bench = list()
         self.used_maps = list()
-        self.registration_opened = True
+        self.registration_opened = False
         self.user_for_giveaway = list()
         self.test = ''
