76ca77364b878b76d504e8760abe3e9d21cb536e
Kane <mormerhav@gmail.com>
2020-05-05 16:54:16 +0300

    Add test for CommunityGamesTeamGenerator#generate_teams_command

--- test_bot/test_cogs/test_community_games_team_generator.py
+++ test_bot/test_cogs/test_community_games_team_generator.py
@@ -13,6 +13,13 @@ async def async_magic():
 MagicMock.__await__ = lambda x: async_magic().__await__()
 
 
+def generate_players_list(length):
+    players_list = []
+    for i in range(length):
+        players_list.append("player" + str(i))
+    return players_list
+
+
 class TestCommunityGamesTeamGenerator(TestCase):
     def test_generate_teams_command_no_allowed_channels(self):
         ctx_mock = MagicMock()
@@ -24,8 +31,19 @@ class TestCommunityGamesTeamGenerator(TestCase):
         self.assertTrue(ctx_mock.send.called)
         self.assertEqual('Not allowed to operate on this channel', ctx_mock.send.call_args[0][0])
 
-    def test_generate_team(self):
-        self.fail()
+    def test_generate_team_less_than_12_players(self):
+        ctx_mock = MagicMock()
+        ctx_mock.message.channel.id = "Community Games"
+        ctx_mock.author.mention = "@Kane"
+        Config.ALLOWED_CHANNEL = ["Community Games"]
+        client_mock = MagicMock()
+        client_mock.global_variables.players_list = generate_players_list(11)
+        comm_games_team_generator = community_games_team_generator.CommunityGamesTeamGenerator(client_mock)
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(comm_games_team_generator.generate_teams_command(comm_games_team_generator, ctx_mock))
+        self.assertTrue(ctx_mock.send.called)
+        self.assertEqual('@Kane Not enough players for 2 teams. Currently have: 11. At least 12 players needed.',
+                         ctx_mock.send.call_args[0][0])
 
     def test_generate_teams(self):
         self.fail()
