ccd944f4cc144ec81774d95d6e769d65a477ab69
Kane <mormerhav@gmail.com>
2020-06-23 01:12:15 +0300

    Improve giveaway.py

--- bot/cogs/giveaway.py
+++ bot/cogs/giveaway.py
@@ -1,55 +1,61 @@
 from random import randint
 
+import discord
 from discord.ext import commands
 
 import Config
 import decorators
 from log import LoggerFactory
+from utils import ListUtils
 
 log = LoggerFactory.get_logger(__name__)
 
 
+# TODO Mor: Add tests
 class Giveaway(commands.Cog):
     def __init__(self, client):
         self.client = client
 
-    @commands.command(name="getGiveawayUser")
-    @decorators.is_admin
-    @decorators.only_allowed_channels
-    async def get_giveaway_user_command(self, ctx):
-        channel = self.client.get_channel(Config.GIVEAWAY_VOICE_CHANNEL)
-        channel_members = channel.members
-        log.debug("Empty the list of participants")
-        del self.client.global_variables.user_for_giveaway[:]
-
-        log.debug("Add user to [user_for_giveaway]")
-        for member in channel_members:
-            self.client.global_variables.user_for_giveaway.append(member.id)
-
-        await ctx.send(
-            "Loaded "
-            + str(len(self.client.global_variables.user_for_giveaway))
-            + " user for the giveaway"
-        )
+    # TODO Mor: Remove this and docs
+    # @commands.command(name="getGiveawayUser")
+    # @decorators.is_admin
+    # @decorators.only_allowed_channels
+    # async def get_giveaway_user_command(self, ctx):
+    #
+    #     log.debug("Empty the list of participants")
+    #     del self.client.global_variables.user_for_giveaway[:]
+    #
+    #     log.debug("Add user to [user_for_giveaway]")
+    #     for member in channel_members:
+    #         self.client.global_variables.user_for_giveaway.append(member.id)
+    #
+    #     await ctx.send(
+    #         "Loaded "
+    #         + str(len(self.client.global_variables.user_for_giveaway))
+    #         + " users for the giveaway"
+    #     )
 
     @commands.command(name="getGiveawayWinner")
     @decorators.is_admin
     @decorators.only_allowed_channels
     async def get_giveaway_winner_command(self, ctx):
-        # get a random number between 0 and the amount of user -1
-        random_index = randint(
-            0, len(self.client.global_variables.user_for_giveaway) - 1
-        )
-        # create a user object out of the drawn id
-        user = self.client.get_user(
-            self.client.global_variables.user_for_giveaway[random_index]
-        )
-        # remove the player from the giveaway list
-        self.client.global_variables.user_for_giveaway.remove(
-            self.client.global_variables.user_for_giveaway[random_index]
-        )
-        # announce the winner
-        await ctx.send(user.mention + " Won!")
+        winners = self.client.global_variables.giveaway_winners
+        channel = self.client.get_channel(Config.GIVEAWAY_VOICE_CHANNEL_ID)
+        channel_members = channel.members
+        contenders = ListUtils.remove_sub_list(channel_members, winners)
+        if not contenders:
+            await ctx.send(embed=discord.Embed(title="Squid Squad Community Games Giveaway",
+                                               description="No contenders!",
+                                               color=discord.Color.blue()))
+            return
+
+        random_index = randint(0, len(contenders) - 1)
+
+        new_winner = contenders[random_index]
+        winners.append(new_winner)
+        await ctx.send(embed=discord.Embed(title="Squid Squad Community Games Giveaway",
+                                           description="And the winner is ... " + new_winner.mention + "!",
+                                           color=discord.Color.blue()))
 
 
 def setup(client):
--- bot/utils/ListUtils.py
+++ bot/utils/ListUtils.py
@@ -2,11 +2,11 @@ def remove_duplicates(lst):
     return list(dict.fromkeys(lst))
 
 
-def add_to_list(lst, list_to_add):
-    lst.extend(list_to_add)
-
-
 # TODO Mor: add tests
+def remove_sub_list(lst, sub_lst):
+    return [elem for elem in lst if elem not in sub_lst]
+
+
 def find_by_id(id_val, lst):
     return next((elem for elem in lst if elem.id == id_val), False)
 
