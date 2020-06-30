c949e6870858654e072d404fb28fc73005060327
Kane <mormerhav@gmail.com>
2020-07-01 00:41:08 +0300

    Add Config.py

---
+++ bot/Config.py
@@ -0,0 +1,17 @@
+# The token needed to contact your bot
+DISCORD_TOKEN = 'NzA2MjI4NTc3MTc1MzM5MTgw.Xq3M0g.Yvq10c7H4m63k7bFV0t4Vt-af_Y'
+
+# Needs to be at the beginning of every command
+COMMAND_PREFIX = '>'
+
+# Names of the allowed channel
+ALLOWED_TEXT_CHANNEL_IDS = [697136698320224286, 696659551063375913]
+
+# Players allowed to use admin commands
+ADMIN_IDS = [277908079688024074, 241239899313930240, 188664785435492352]
+
+# Channel where the giveaway happens
+GIVEAWAY_VOICE_CHANNEL_ID = 590976878634860554
+
+# Community Games voice channel
+COMMUNITY_GAMES_VOICE_CHANNEL_ID = 590976878634860554
